2b398e4d1f1d0e015e5cc92392c60fe07110cdf2
ArghaGhosh1 <arghaghosh276@hotmail.com>
2026-04-20 23:53:45 +0530

    hi

---
+++ app/src/main/java/com/example/whatsappclone/Model/phoneAuthData.kt
@@ -0,0 +1,10 @@
+package com.example.whatsappclone.Model
+
+data class phoneAuthData(
+
+    val userId: String = "",
+    val phoneNumber: String = "",
+    val name: String = "",
+    val status: String = "",
+    val profileImage: String? = ""
+)
