8293963557efe4df247a4ef0f1e58c8c7f2e5359
ArghaGhosh1 <arghaghosh276@hotmail.com>
2026-04-23 02:35:25 +0530

    hi

---
+++ app/src/main/java/com/example/whatsappclone/presentation/callScreen/recentCallsList.kt
@@ -0,0 +1,9 @@
+package com.example.whatsappclone.presentation.callScreen
+
+data class recentCallsList(
+
+    val name: String,
+    val image: Int,
+    val isMissedCall: Boolean,
+    val time: String
+)
