1120b9926be3d9a451e91e293726c04f616744c6
ArghaGhosh1 <arghaghosh276@hotmail.com>
2026-04-25 23:24:34 +0530

    hi

--- app/src/main/java/com/example/whatsappclone/presentation/viewModel/phoneAuthViewMOdel.kt
+++ app/src/main/java/com/example/whatsappclone/presentation/viewModel/phoneAuthViewMOdel.kt
@@ -1,12 +1,15 @@
 package com.example.whatsappclone.presentation.viewModel
 
 import android.app.Activity
+import android.content.Context
 import android.util.Log
+import android.util.TimeUtils
 import androidx.lifecycle.ViewModel
 import com.example.whatsappclone.Model.phoneAuthData
 import com.google.firebase.FirebaseException
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.PhoneAuthCredential
+import com.google.firebase.auth.PhoneAuthOptions
 import com.google.firebase.auth.PhoneAuthProvider
 import com.google.firebase.crashlytics.buildtools.reloc.org.apache.http.auth.AuthState
 import com.google.firebase.database.FirebaseDatabase
@@ -14,6 +17,7 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import jakarta.inject.Inject
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import java.util.concurrent.TimeUnit
 
 
 @HiltViewModel
@@ -21,14 +25,15 @@ class phoneAuthViewMOdel @Inject constructor(
 
     private val firebaseAuth: FirebaseAuth,
     private val database: FirebaseDatabase
-): ViewModel() {
+) : ViewModel() {
 
-    private val _authState = MutableStateFlow<AuthState>(com.example.whatsappclone.presentation.viewModel.AuthState.Ideal)
+    private val _authState =
+        MutableStateFlow<AuthState>(com.example.whatsappclone.presentation.viewModel.AuthState.Ideal)
     val authState = _authState.asStateFlow()
 
     private val userRef = database.reference.child("users")
 
-    fun sendVarificationCode( phoneNumber:String, activity: Activity){
+    fun sendVarificationCode(phoneNumber: String, activity: Activity) {
 
         _authState.value = com.example.whatsappclone.presentation.viewModel.AuthState.Loading
 
@@ -37,34 +42,96 @@ class phoneAuthViewMOdel @Inject constructor(
             override fun onCodeSent(id: String, token: PhoneAuthProvider.ForceResendingToken) {
                 super.onCodeSent(id, token)
 
-                Log.d("phoneVarification","onCodeSent: $id")
+                Log.d("phoneVarification", "onCodeSent: $id")
 
-                _authState.value = com.example.whatsappclone.presentation.viewModel.AuthState.CodeSent(verificationId = id)
+                _authState.value =
+                    com.example.whatsappclone.presentation.viewModel.AuthState.CodeSent(
+                        verificationId = id
+                    )
             }
-
 
 
             override fun onVerificationCompleted(credential: PhoneAuthCredential) {
-                TODO("Not yet implemented")
+
+                signInWithCredentials(credential, context = activity)
+
             }
 
             override fun onVerificationFailed(exception: FirebaseException) {
-                Log.d("phoneAuth","varification failed: ${exception.message}")
-                _authState.value = com.example.whatsappclone.presentation.viewModel.AuthState.error(exception.message ?: "varification failed" )
+                Log.d("phoneAuth", "varification failed: ${exception.message}")
+                _authState.value = com.example.whatsappclone.presentation.viewModel.AuthState.error(
+                    exception.message ?: "varification failed"
+                )
+            }
+
+        }
+
+        val phoneAuthOptions = PhoneAuthOptions.newBuilder(firebaseAuth)
+            .setPhoneNumber(phoneNumber)
+            .setTimeout(60L, TimeUnit.SECONDS)
+            .setActivity(activity)
+            .setCallbacks(option)
+            .build()
+
+        PhoneAuthProvider.verifyPhoneNumber(phoneAuthOptions)
+
+    }
+
+    private fun signInWithCredentials(credential: PhoneAuthCredential, context: Context) {
+
+        _authState.value = com.example.whatsappclone.presentation.viewModel.AuthState.Loading
+
+        firebaseAuth.signInWithCredential(credential).addOnCompleteListener { task ->
+
+            if (task.isSuccessful) {
+                val user = firebaseAuth.currentUser
+                val phoneAuthUser = phoneAuthData(
+
+                    userId = user?.uid ?: "",
+                    phoneNumber = user?.phoneNumber ?: ""
+
+                )
+
+                markUserAsSignedIn(context)
+                _authState.value =
+                    com.example.whatsappclone.presentation.viewModel.AuthState.succes(phoneAuthUser)
+
+                fetchUserProfile(user?.uid ?: "")
+            } else {
+                _authState.value = com.example.whatsappclone.presentation.viewModel.AuthState.error(
+                    task.exception?.message ?: "signIn failed"
+                )
             }
 
         }
 
 
+    }
+
+    private fun markUserAsSignedIn(context: Context){
+
+        val sharedPreferences = context.getSharedPreferences("app_prefs", Context.MODE_PRIVATE)
+        sharedPreferences.edit().putBoolean("isSignedIn",true).apply()
+    }
+
+    private fun fetchUserProfile(userId: String){
+
+        val userRef = userRef.child(userId)
+        userRef.get().addOnSuccessListener { snapshot ->
+
+            if(snapshot != null){
+                val userProfile = snapshot.getValue(phoneAuthData :: class.java)
+            }
+        }
 
     }
 }
 
-sealed class AuthState(){
+sealed class AuthState() {
 
-    object Ideal: AuthState()
-    object Loading: AuthState()
-    data class CodeSent(val verificationId: String): AuthState()
-    data class succes(val user: phoneAuthData): AuthState()
-    data class error(val message: String): AuthState()
+    object Ideal : AuthState()
+    object Loading : AuthState()
+    data class CodeSent(val verificationId: String) : AuthState()
+    data class succes(val user: phoneAuthData) : AuthState()
+    data class error(val message: String) : AuthState()
 }
