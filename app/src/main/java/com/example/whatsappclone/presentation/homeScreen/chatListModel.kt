2e6c8f348221ba9e4e598249c874dbe46e03d2a4
ArghaGhosh1 <arghaghosh276@hotmail.com>
2026-04-28 04:29:33 +0530

    hi

---
+++ app/src/main/java/com/example/whatsappclone/presentation/homeScreen/chatListModel.kt
@@ -0,0 +1,9 @@
+package com.example.whatsappclone.presentation.homeScreen
+
+data class chatListModel(
+
+    val image: Int,
+    val name: String,
+    val time: String,
+    val message: String
+)
